2347c74309d62f48ad6cf566f54e7fb880465260
myishamoore <62115695+myishamoore@users.noreply.github.com>
2020-04-23 17:03:03 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.component{
+  render() {
+    return(
+      <div> 
+      <p> hello World </p>
+      
+      </div>
+
+    );
+  }
+}
+
+
+
+export default App;
